573b88f402b64db53e61b594a9f8b4315398de08
seungh0 <will.seungho@gmail.com>
2020-05-05 02:39:48 +0900

    Initial commit

---
+++ src/test/java/will/seungho/jpastudy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package will.seungho.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
