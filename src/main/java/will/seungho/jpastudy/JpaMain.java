4fed926b65e64640157f6ad22cb93b0716decaed
seungh0 <will.seungho@gmail.com>
2020-05-06 17:10:40 +0900

    플러시

--- src/main/java/will/seungho/jpastudy/JpaMain.java
+++ src/main/java/will/seungho/jpastudy/JpaMain.java
@@ -5,6 +5,7 @@ import will.seungho.jpastudy.member.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
 import javax.persistence.Persistence;
 
 /**
@@ -22,20 +23,37 @@ public class JpaMain {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
 		try {
-			Member member = entityManager.find(Member.class, 150L);
-			member.setName("변경감지 테스트");
-			System.out.println("============");
+			Member member = new Member(202L, "Member200");
+			entityManager.persist(member);
+			entityManager.flush();
+			System.out.println("+===========================");
 
 			/**
-			 * 변경감지 (Dirty Checking)
-			 * 값만 바꿧는데? UPDATE QUERY가 나간다!
+			 * flush를 해도 1차 캐쉬는 그대로 유지한다.
+			 * (영속성 컨텍스트를 비우지 않음!!)
 			 *
-			 * flush() 되면
-			 * 엔티티와 스냅샷을 비교한다!
+			 * 영속성 컨텍스트의 변경내용을 데이터베이스에 동기화.
 			 *
-			 * 변경이 있으면 UPDATE QUERY를 쓰기 지연 SQL 저장소에 저장
-			 * 커밋떄 UPDATE QUERY 날라감~
+			 * 영속성 컨텍스트 내의 쓰기지연 SQL 저장소의 쿼리들이 나간다.
+			 * 변경감지.
+			 *
+			 * 영속성 컨텍스틀 플러시하는 3가지 방법
+			 * 1. entityManager.flush() 직접 호출
+			 * 2. 트랜잭션 커밋
+			 * 3. JPQL 쿼리 실행
+			 *
+			 * cf) JPQL 쿼리 실행시 플러시가 자동으로 호출 되는 이유
+			 * 예를 들어 검색하려면 DB에서 가져와야하는데... 그 전에 반영을 해야함!
 			 */
+
+//			entityManager.setFlushMode(FlushModeType.AUTO);
+//			entityManager.setFlushMode(FlushModeType.COMMIT);
+			/**
+			 * 플러시 모드 옵션
+			 * AUTO = 커밋, 쿼리 실행시 플러시(기본값)
+			 * COMMIT = 커밋할 대만 플러시
+			 */
+
 
 			transaction.commit();
 		} catch (Exception e) {
