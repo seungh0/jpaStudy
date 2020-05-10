612331f82952594ecdbc990cb8c33c799fda2556
seungh0 <will.seungho@gmail.com>
2020-05-10 19:19:32 +0900

    [연관관계] 양방향 연관관계와 연관관계의 주인 - 팁3
    1. 양방향 매핑 엔티티에는 @ToString() 정의시 무한루프에 빠질 수 있다.
    2. Controller에서 절대 엔티티를 직접 반환하면 안되는 이유
    - 무한 루프에 빠질 수 있다(JSON 생성 라이브러리 - 양방향 매핑)
    - 엔티티가 변경되면 API도 바뀌어버림.!!

--- src/main/java/will/seungho/jpastudy/member/Member.java
+++ src/main/java/will/seungho/jpastudy/member/Member.java
@@ -12,6 +12,21 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
+/**
+ * 양방향 매핑시 무한루프를 조심하자!!!
+ * toString(), lombok, JSON 생성 라이브러리
+ */
+
+/**
+ * 아 그래서 ToString() 양방향 매핑 엔티티에 정의 안하고...
+ * (Controller에서 절대 엔티티를 반환하지 않아야하는 이유!!!!
+ * 그래서 엔티티를 DTO로 반환후 Controller에서 반환한다.
+ * Controller에서 Entity를 쓸때의 문제점
+ * 1. 문제 무한루프 가능
+ * 2. 엔티티는 변경될 수 있음 근데, 엔티티를 변경하는 순간 API가 바뀌어버림
+ */
+
 @Getter
 @NoArgsConstructor
 @Entity
