06131ad162e092d9889c28433e516aa6afe85c23
seungh0 <will.seungho@gmail.com>
2020-05-10 19:04:50 +0900

    [연관관계] 양방향 연관관계와 연관관계의 주인 - 팁

--- src/main/java/will/seungho/jpastudy/JpaMain.java
+++ src/main/java/will/seungho/jpastudy/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,25 +18,47 @@ public class JpaMain {
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
 		try {
-			Member member = Member.builder()
-					.name("seungho")
-					.build();
-			entityManager.persist(member);
-
 			Team team = Team.builder()
 					.name("teamA")
 					.build();
-			team.getMembers().add(member); // Member.teamID은 null
 			entityManager.persist(team);
 
+			Member member = Member.builder()
+					.name("seungho")
+					.team(team)
+					.build();
+			entityManager.persist(member); // 1차 캐시 저장
+
 			/**
-			 * Why?
-			 *
-			 * 연관관계 주인이 아닌 Team.member (mappedBy)은 읽기만 가능하다.
-			 * 따라서 team.getMembers().add(member) 해봤자
-			 * 안들어감!
+			 * 객체지향적으로 생각하면 양쪽에 모두 값을 넣어줘야한다!!
+			 * 안넣어줘도 아래처럼 검색 가능함!
+			 * but
+			 * flush(), clear()를 안해주면
+			 */
+			team.getMembers().add(member); // 읽기 전용! 둘다 매핑해주자 그냥
+//			entityManager.flush();
+//			entityManager.clear();
+
+			/**
+			 * 지연로딩.
+			 * Team.members를 실제로 사용하는 시점에 쿼리를 한번 날림.
+			 */
+			Team findTeam = entityManager.find(Team.class, team.getId());
+			/**
+			 * 1차 캐시에 있음 => 그래서 team.getMembers().add(member) 안해주고
+			 * 플러쉬 clear로 안해주면 안나옴 !
+			 * 고래서 고냥 양쪽으로 값을 세팅해주자!
 			 */
 
+			/**
+			 * 테스트 케이스 작성할떄,
+			 * JPA와 분리해서 작성하는데
+			 * null이 나오니깐 그냥 양방향 연관관계 매핑시 양쪽에다 모두 값을 세팅해주자!!!!!
+			 */
+			List<Member> memberList = findTeam.getMembers();
+			for (Member m : memberList) {
+				System.out.println(m.getName());
+			}
 			transaction.commit();
 		} catch (Exception e) {
 			entityManager.close();
